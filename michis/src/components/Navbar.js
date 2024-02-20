1677c47da7afd7e88239b7df6084ffc3ee7cdbed
holaclaris <mamutchiquitito@gmail.com>
2024-02-20 12:21:03 -0300

    Mejora carrito y contador

--- michis/src/components/Navbar.js
+++ michis/src/components/Navbar.js
@@ -7,6 +7,7 @@ import { IoCartOutline } from "react-icons/io5";
 function Navbar(){
     return(
         <nav>
+            <div className="barrasLinks">
             <button className='barras'>
                 <FaBars />
             </button>
@@ -19,9 +20,11 @@ function Navbar(){
                     <Link to='/Contact' className='links'> CONTACTO </Link>
                 </li>
             </ul>
+            </div>
 
                 <div className='carrito'>
                     <Link to='/Carrito' className='links'> <IoCartOutline id='carritoCompras' /> </Link>
+                    <div id='contador'><p>0</p></div>
                 </div>
         </nav>
     )
